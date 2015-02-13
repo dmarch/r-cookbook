f3d186aeb70d5f7fb7f8577b4cb0ad569805bac6
dmarch <damamo82@gmail.com>
2015-02-13 15:28:36 +0100

    str2deg fix NA problem

--- geo/str2deg.R
+++ geo/str2deg.R
@@ -1,19 +1,21 @@
 ########################################################################################
-# Convert data formated in degrees, minutes and seconds to decimal degrees
+# Convert data formated in degrees, minutes and seconds (DMS) to decimal degrees
 # Author: David March
-# Contact: dmarch@socib.es
+# Contact: david@imedea.uib-csic.es
 #
 # Rationale:
-# Most libraries convert DMS to degrees when DMS are found in different columns.
-# However, DMS format is often found in a single attribute column and multiple formats.
-# This scripts converts text strings into numeric decimal degrees
+# DMS format is commonly used for geo-refenreced data. However,
+# the notation format can be different between data sources, and splitting elements can be a pain.
+# str2deg function aims to make this data manipulation more easy
+#
+# Note:
+# This is a beta version, since not all possibilities have been tested yet. Please, report
+# any malfunction.
 ########################################################################################
 
 
-rm(list=ls(all=TRUE)) #reset data
-
 #--------------------------------------------------------------------------------
-# str2deg   String of DegreesMinutesSeconds to decimal degrees
+# str2deg   Transform string in DMS format to decimal degrees
 #--------------------------------------------------------------------------------
 str2deg <- function(x,format=c(deg="º", min="'", sec="''", dir="[NSWE]")){
   # Arguments:
@@ -29,7 +31,7 @@ str2deg <- function(x,format=c(deg="º", min="'", sec="''", dir="[NSWE]")){
   require(stringr)
   require(gmt)
   
-  # Identify location of markers
+  # Identify position of markers
   d.pos <- str_locate(x, format[["deg"]])[,1]
   m.pos <- str_locate(x, format[["min"]])[,1]
   s.pos <- str_locate(x, format[["sec"]])[,1]
@@ -43,10 +45,10 @@ str2deg <- function(x,format=c(deg="º", min="'", sec="''", dir="[NSWE]")){
 
   # Convert to DMS standard form from GMT library
   dms <- paste(paste(dd,mm,ss,sep=":"),C,sep="")
+  dms <- str_replace(dms, "NA", 0)
   
   # Convert DMS to decimal degrees
   output<- deg2num(dms)
-  #attr(output,"names")<-NULL
   output
 }
 #--------------------------------------------------------------------------------
