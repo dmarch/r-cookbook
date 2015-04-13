cd86c539b9a32efaddc8938bd3d952867986da08
dmarch <damamo82@gmail.com>
2015-04-13 15:22:54 +0200

    example in esriocean has been commented

--- color/esriOcean.R
+++ color/esriOcean.R
@@ -1,7 +1,44 @@
 esri.ocean <- function(scale="small", breaks=FALSE, alpha=1){
-  # This function provides color palette and breakpoints used in the ESRI Ocean Basemap
+  #
+  # Description
+  #
+  # This function provides color palette or breakpoints used in the ESRI Ocean Basemap
   # Source: http://blogs.esri.com/esri/arcgis/2012/08/02/esri-ocean-basemap-color-style-available-for-download/
-  
+  #
+  #
+  # Arguments
+  #
+  # scale: Scale of the map to use more or less colors. Accepts "small" or "medium"
+  # breaks: If FALSE function returns the colors, if TRUE function returns the bathymetric breaks.
+  # alpha: transparency, between 0 and 1
+  #
+  #
+  # TODO:
+  # validate arguments
+  #
+  #  
+  # Example
+  #
+  # library(raster)
+  # library(marmap)
+  # data(hawaii)
+  #  
+  # # transform DEM to raster
+  # rmap <- marmap::as.raster(hawaii)             
+  #
+  # # create hillshae
+  # slope <- terrain(rmap, opt='slope')
+  # aspect <- terrain(rmap, opt='aspect')
+  # hill <- hillShade(slope, aspect, 45, 270)
+  #  
+  # # plot
+  # png(filename="bati.png",width=1900, height=1450, res= 150)
+  # plot(hill, col=grey(0:100/100), legend=FALSE, asp=1)
+  # plot(rmap, breaks=esri.ocean(scale="medium",breaks=TRUE),
+  #     col=esri.ocean(scale="medium", alpha=0.5),
+  #     legend=FALSE, add=TRUE)
+  # dev.off()
+   
   if(alpha<0 | alpha>1) stop("alpha is defined between 0 and 1")
   
   if (scale=="medium"){
@@ -25,26 +62,5 @@ esri.ocean <- function(scale="small", breaks=FALSE, alpha=1){
 }
 
 
-### Example
 
-library(raster)
-library(marmap)
-data(hawaii)
-
-# transform DEM to raster
-rmap <- marmap::as.raster(hawaii)             
-
-# create hillshae
-slope <- terrain(rmap, opt='slope')
-aspect <- terrain(rmap, opt='aspect')
-hill <- hillShade(slope, aspect, 45, 270)
-
-# plot
-setwd("~/Git/r-cookbook/color")
-png(filename="bati.png",width=1900, height=1450, res= 150)
-plot(hill, col=grey(0:100/100), legend=FALSE, asp=1)
-plot(rmap, breaks=esri.ocean(scale="medium",breaks=TRUE),
-     col=esri.ocean(scale="medium", alpha=0.5),
-     legend=FALSE, add=TRUE)
-dev.off()
 
